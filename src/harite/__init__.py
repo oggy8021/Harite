a171347827b921342a2b2f14df6b626e59a1a1dc
oggy_ <oggy_@example.com>
2026-03-12 14:45:49 +0900

    fix: explicit re-export of plugins to satisfy linter

--- src/harite/__init__.py
+++ src/harite/__init__.py
@@ -2,4 +2,4 @@
 __version__ = "0.0.0"
 
 from .cli import run as run  # expose run for console_scripts
-from . import plugins  # expose plugins for discovery
+from . import plugins as plugins  # expose plugins for discovery (explicit re-export for linters)
