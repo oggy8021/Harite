a67fd58301d2f24684c3f98f23b23b134f772f5f
oggy_ <oggy_@example.com>
2026-04-12 23:00:18 +0900

    chore: prepare release v0.1.2

--- src/harite/__init__.py
+++ src/harite/__init__.py
@@ -1,5 +1,5 @@
 """Harite package init."""
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .cli import run as run  # expose run for console_scripts
 from . import plugins as plugins  # expose plugins for discovery (explicit re-export for linters)
