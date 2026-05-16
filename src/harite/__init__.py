8f507205708fc8d1b0ba68434ed7fc8720e6d9dc
oggy_ <oggy_@example.com>
2026-05-16 10:56:12 +0900

    Bump version to 0.1.3

--- src/harite/__init__.py
+++ src/harite/__init__.py
@@ -1,5 +1,5 @@
 """Harite package init."""
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .cli import run as run  # expose run for console_scripts
 from . import plugins as plugins  # expose plugins for discovery (explicit re-export for linters)
